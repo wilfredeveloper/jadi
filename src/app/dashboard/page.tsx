2c7a5e746cb9642e0b4fb5ede5583a71891f470d
wilfredeveloper <pxumtech@gmail.com>
2024-02-26 12:52:12 +0300

    Moved the app directory into src folder

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,24 +2,22 @@ import {LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
 import { fetchUserData, isNewUser } from "../utils/userUtils";
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 
-
-
 export default async function Page() {
     const { isAuthenticated } = getKindeServerSession();
     const userData = await fetchUserData();
 
 
     return ( await isAuthenticated()) ? (
-        <main>
-            <pre>
-                {JSON.stringify(userData, null, 2)}
-            </pre>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
             <div>
                 {await isNewUser() ? (
                     <div>Welcome to onboarding</div>
                 ): (
-                    <div>Welcome back {userData?.given_name}</div>
+                    <div>
+                        <div>Welcome back {userData?.given_name}</div>
+                        <pre>{ JSON.stringify(userData, null, 2)}</pre>
+                    </div>
                 )}
             </div>
             <LogoutLink>Log out</LogoutLink>
