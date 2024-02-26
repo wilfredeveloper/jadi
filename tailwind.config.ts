756b6d395bec71db1629e858d7258efee0e22ebe
wilfredeveloper <pxumtech@gmail.com>
2024-02-26 21:37:58 +0300

    💄 UI Update - [ Initial commit ] - Structured a good landing page and dashboard together with a nav component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,26 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '0.5rem',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#ffffff",
+        background: "#F9F9F9",
+        destructive: "#FF0000",
+        foreground: "#000000",
       },
     },
   },
