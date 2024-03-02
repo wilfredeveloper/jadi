6a986331383cc47845e8a506959b965c9407a32a
wilfredeveloper <pxumtech@gmail.com>
2024-03-02 23:56:36 +0300

    Refactor dashboard page to improve authentication and data fetching

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,45 +1,37 @@
-import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { fetchUserData, fetchUserId } from "../utils/userUtils";
+import { fetchUserId } from "../utils/userUtils";
 import { DashLayout } from "@/components/component/dash-layout";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ErrorPageProtectedLayout } from "@/components/component/ProtectedLayout/error-page-protected-layout";
-import { DashLoader } from "@/components/component/dash-loader";
 
 export default async function Protected() {
   const { isAuthenticated } = getKindeServerSession();
-  const userID = (await fetchUserId()) || "";
+  
+  if (!(await isAuthenticated())) {
+    return <ErrorPageProtectedLayout />;
+  }
+  
+  async function fetchUserData() {
+    "use server"
+    const userID = (await fetchUserId()) || "";
+    const response = await fetch(
+      `http://localhost:3000/api/onboard?id=${userID}`
+    );
+    const userData = await response.json();
+    return userData;
+  }
 
+  const userData = await fetchUserData();
 
-  const response = await fetch(
-    `http://localhost:3000/api/onboard?id=${userID}`
-  );
-
-  const userData = await response.json();
-
-  return (await isAuthenticated()) ? (
-    // 1️⃣ If the user is authenticated, display the user's data
+  return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {userData ? (
-          <DashLayout
-            userData={{
-              family_name: userData.lastName,
-              given_name: userData.firstName,
-              picture: userData.picture,
-              email: userData.preferredEmail,
-            }}
-          />
-        ) : (
-          <div>
-            <DashLoader />
-          </div>
-        )}
-      </div>
-    </main>
-  ) : (
-    // 2️⃣ If the user is not authenticated, display a message to prompt the user to log in
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ErrorPageProtectedLayout />
+      <DashLayout
+        userData={{
+          family_name: userData.lastName,
+          given_name: userData.firstName,
+          picture: userData.picture,
+          email: userData.preferredEmail,
+        }}
+      />
     </main>
   );
 }
