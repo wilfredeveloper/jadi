6db54390bbf15c36a4ff4117339d97dde05effad
wilfredeveloper <pxumtech@gmail.com>
2024-03-06 15:12:50 +0300

    Refactor dashboard page component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,21 @@
-import { fetchBasicUserData, fetchComprehensiveUserData } from "../utils/userUtils";
+import {
+  fetchBasicUserData,
+  fetchComprehensiveUserData,
+} from "../utils/userUtils";
 import { DashLayout } from "@/components/component/dash-layout";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ErrorPageProtectedLayout } from "@/components/component/ProtectedLayout/error-page-protected-layout";
 
 // 🛡️ This is a protected component, it will only render if the user is authenticated
-export default async function Protected() {
+export default async function Page() {
   // 🔐 Checking if the user is authenticated using Kinde server session
   const { isAuthenticated } = getKindeServerSession();
-  
-  
 
   // 📥 Fetching the user data
   const userData = await fetchBasicUserData();
 
   // 🎨 Rendering the main layout with user data
-  return ( await isAuthenticated()) ? (
+  return (await isAuthenticated()) ? (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <DashLayout
         userData={{
@@ -27,5 +28,5 @@ export default async function Protected() {
     </main>
   ) : (
     <ErrorPageProtectedLayout />
-  )
+  );
 }
