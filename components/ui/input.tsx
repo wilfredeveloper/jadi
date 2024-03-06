b913ed4892384bf25b2290cbb33f5d73263427e1
wilfredeveloper <pxumtech@gmail.com>
2024-03-06 22:56:25 +0300

    Add name prop to Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -6,6 +6,7 @@ interface BaseInputProps {
   variant?: "text" | "dropdown";
   type?: string;
   className?: string;
+  name?: string;
 }
 
 type TextInputProps = BaseInputProps &
@@ -20,7 +21,7 @@ export interface InputProps
 const Input = React.forwardRef<
   HTMLInputElement | HTMLSelectElement,
   TextInputProps | DropdownInputProps
->(({ className, variant, type, ...props }, ref) => {
+>(({ className, variant, type, name, ...props }, ref) => {
   if (variant === "dropdown") {
     const dropdownProps = props as DropdownInputProps;
     return (
@@ -29,6 +30,7 @@ const Input = React.forwardRef<
           "flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-gray-300",
           className
         )}
+        name={name}
         ref={ref as React.Ref<HTMLSelectElement>}
         {...dropdownProps}
       >
@@ -51,6 +53,7 @@ const Input = React.forwardRef<
         "flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:ring-offset-gray-950 dark:placeholder:text-gray-400 dark:focus-visible:ring-gray-300",
         className
       )}
+      name={name}
       ref={ref as React.Ref<HTMLInputElement>}
       {...(props as TextInputProps)}
     />
