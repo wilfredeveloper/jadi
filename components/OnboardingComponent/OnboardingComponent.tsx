d01b4ccb2ac1bdc805e14a48d2cd517b7d453561
wilfredeveloper <pxumtech@gmail.com>
2024-03-06 23:21:35 +0300

    Fix form submission in OnboardingComponent

--- components/OnboardingComponent/OnboardingComponent.tsx
+++ components/OnboardingComponent/OnboardingComponent.tsx
@@ -112,7 +112,25 @@ const handleInterestSelect = (interest: string) => {
     visible: { opacity: 1 },
   };
 
+  
   const [state, formAction] = useFormState(createUser, initialState);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    const formData = new FormData();
+    formData.append('given_name', userData?.given_name || "")
+    formData.append('family_name', userData?.family_name || "")
+    formData.append('email', userData?.email || "")
+    formData.append('id', userData?.id || "")
+    formData.append('picture', userData?.picture || "")
+    formData.append('major', major);
+    formData.append('institution', institution);
+    formData.append('inputInterests', JSON.stringify(inputInterests));
+    formData.append('selectedInterests', JSON.stringify(selectedInterests));
+  
+    await createUser(state, formData);
+  };
 
   return (
     <div className={`${styles.wrapper}`}>
@@ -122,7 +140,7 @@ const handleInterestSelect = (interest: string) => {
         value={(currentCard / totalSteps) * 100}
       />
 
-      <form action={formAction}>
+      <form action={formAction} onSubmit={handleSubmit}>
         <motion.div
           initial="hidden"
           animate="visible"
