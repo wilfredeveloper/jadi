ee589027cda33ea9169611b997496810282a784f
wilfredeveloper <pxumtech@gmail.com>
2024-03-07 01:17:20 +0300

    Add user profile data to createUser function

--- src/app/actions/onboardingActions.ts
+++ src/app/actions/onboardingActions.ts
@@ -1,4 +1,7 @@
 "use server";
+import { db } from "@/src/db"
+import { UserProfiles } from "@/src/db/schema"
+
 
 export async function createUser(
   prevState: { message: string },
@@ -7,10 +10,27 @@ export async function createUser(
 
   const major = formData.get('major')?.toString();
   const email = formData.get('email')?.toString();
+  const profilePhotoURL = formData.get('profilePhotoURL')?.toString();
+  const kinde_ID = formData.get('kinde_ID')?.toString();
+  const firstName = formData.get('firstName')?.toString();
+  const lastName = formData.get('lastName')?.toString();
   const institution = formData.get('institution')?.toString();
+  const inputInterests = formData.getAll('inputInterests').map((interest) => JSON.parse(interest.toString()));
+  const selectedInterests = formData.getAll('selectedInterests').map((interest) => JSON.parse(interest.toString()));
+  const interests = [...inputInterests.flat(), ...selectedInterests.flat()];
+
+  const userDataModel = {
+    kindeID: kinde_ID,
+    firstName,
+    lastName,
+    email,
+    coursemajor: major,
+    interests,
+    profilePhotoURL,
+  }
 
   console.log("\nCreating user with the following data:");
-  console.log({ major, email, institution });
+  console.log(userDataModel);
 
   return { message: "User created" };
 }
