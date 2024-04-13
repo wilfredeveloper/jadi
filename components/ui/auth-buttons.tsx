4d359ea059014c17b167833991879cfe1f88d6e8
wilfredeveloper <pxumtech@gmail.com>
2024-04-13 12:49:52 +0300

    Update AuthButtonsGroup component to use dynamic postLoginRedirectURL

--- components/ui/auth-buttons.tsx
+++ components/ui/auth-buttons.tsx
@@ -1,16 +1,20 @@
+"use client"
+import { usePathname } from 'next/navigation'
 import { Button } from "./button";
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components"
+import { LoginLink, RegisterLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components"
+
 
 export default function AuthButtonsGroup() {
+  const pathname = usePathname();
   return (
     <>
-      <LoginLink postLoginRedirectURL="/dashboard">
+      <LoginLink postLoginRedirectURL={`${pathname}`}>
         <Button size={"sm"} className="mx-2" variant={"outline"}>
           Sign in
         </Button>
       </LoginLink>
 
-      <RegisterLink postLoginRedirectURL="/onboarding">
+      <RegisterLink postLoginRedirectURL={`${pathname}`}>
         <Button size={"sm"} className="mx-2">
           Create Account
         </Button>
