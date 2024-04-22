e5abb6e9e18a13c0141308ffa8c40558e536c4e3
wilfredeveloper <pxumtech@gmail.com>
2024-04-22 04:16:04 +0300

    Update Firestore client configuration to use environment variables

--- src/app/config/firestore-client.js
+++ src/app/config/firestore-client.js
@@ -2,19 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore/lite";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import "dotenv/config"
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB4jzBqIsYgsWzOYq_cvc6hmUIUlpektW4",
-  authDomain: "jadiplatform-f1425.firebaseapp.com",
-  projectId: "jadiplatform-f1425",
-  storageBucket: "jadiplatform-f1425.appspot.com",
-  messagingSenderId: "810594798926",
-  appId: "1:810594798926:web:52fcce7f62efd6d427a21f",
-  measurementId: "G-PM035TEL85"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
