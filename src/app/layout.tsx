11be46aff7cc2d49bd77602b3cd118dc7b134edb
wilfredeveloper <pxumtech@gmail.com>
2024-04-26 14:01:32 +0300

    Refactor UI styles and components, add new features, and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/component/NavBar/NavBar";
 import { Toaster } from "@/components/ui/toaster"
+import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
