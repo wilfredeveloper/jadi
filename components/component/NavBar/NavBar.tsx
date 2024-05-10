3a65afa7f0541b2a1b01a1b6b8df6a7f5216a555
wilfredeveloper <pxumtech@gmail.com>
2024-05-10 23:44:19 +0300

    Changed from server component to client component

--- components/component/NavBar/NavBar.tsx
+++ components/component/NavBar/NavBar.tsx
@@ -3,33 +3,27 @@
  * @see https://v0.dev/t/opuAhWs3RF2
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
-"use client";
-
-import { usePathname } from "next/navigation";
 import Logo from "@/components/ui/logo";
 import NavLinks from "@/components/ui/nav-links";
 import styles from "./Navbar.module.css";
-import { useState } from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Component() {
-  const pathname = usePathname();
-  const isdashboard = pathname === "/dashboard";
-  const [isNavClosed, setisNavClosed] = useState(false);
+export default async function Component() {
+  const isAuthenticated = getKindeServerSession().isAuthenticated();
 
   return (
-    <aside className={`${styles.navbar} ${isNavClosed && `${styles.navbar_closed} full-width`}`}>
+    <aside className={`${styles.navbar} full-width`}>
       <div className={`${styles.logo_wrapper}`}>
         <Logo className={`${styles.logo}`} width="32" height="52" />
       </div>
 
-      <button
-        className={`${styles.toggle_btn}`}
-        onClick={() => setisNavClosed(!isNavClosed)}
-      >
-        {isNavClosed ? <DotsIcon /> : <ChevronIconLeft />}
-      </button>
+      <button className={`${styles.toggle_btn}`}></button>
 
-      {!isdashboard && <NavLinks />}
+      {(await isAuthenticated) ? (
+        <NavLinks isUserAuthenticated={true} />
+      ) : (
+        <NavLinks isUserAuthenticated={false} />
+      )}
     </aside>
   );
 }
