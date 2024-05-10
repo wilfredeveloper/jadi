48741bca348b0e84b4f57e0ff78733a98214f833
wilfredeveloper <pxumtech@gmail.com>
2024-05-10 23:46:07 +0300

    chore: Update AlgoliaService to save and update records
    This commit updates the AlgoliaService to include two new functions: `saveToAlgolia` and `updatePartialAlgoliaData`. The `saveToAlgolia` function saves a record to Algolia, while the `updatePartialAlgoliaData` function updates a record in Algolia with partial data. These changes improve the functionality of the service and ensure that records are properly saved and updated in Algolia.

--- src/app/services/algoliaService.ts
+++ src/app/services/algoliaService.ts
@@ -1,31 +1,40 @@
 import index from "../config/algolia-client";
 
 interface Data {
-  url: string;
-  size: number | null;
-  name: string | null;
-  mimeType: string | null;
-  extension: string | null;
-  cms_id: string | null;
-  createdAt: string | null;
-  updatedAt: string | null;
-  noteTitle: string;
-  Tags: string[];
-  category: string;
-  description: string;
-  upvotes: any[];
-  views: number;
-  popularity: number;
-  saves: any[];
-  likes: any[];
+  objectID: string;
+  cms_id?: string | null;
+  url?: string;
+  size?: number | null;
+  name?: string | null;
+  mimeType?: string | null;
+  extension?: string | null;
+  createdAt?: string | null;
+  updatedAt?: string | null;
+  noteTitle?: string;
+  Tags?: string[];
+  category?: string;
+  description?: string;
+  upvotes?: number;
+  views?: number;
+  popularity?: number;
+  saves?: number;
+  likes?: string[];
 }
 
 export const saveToAlgolia = async (data: Data) => {
   try {
-        const { objectIDs } = await index
-            .saveObjects([data], { autoGenerateObjectIDIfNotExist: true });
-        return console.log("\n[ALGOLIA]🎉 RUpdated record Index", objectIDs);
-    } catch (error) {
-        return console.error("\n[ALGOLIA]🚨 Error updating record Index: ", error);
-    }
+    const { objectIDs } = await index.saveObjects([data]);
+    return console.log("\n[ALGOLIA]🎉 RUpdated record Index", objectIDs);
+  } catch (error) {
+    return console.error("\n[ALGOLIA]🚨 Error updating record Index: ", error);
+  }
+};
+
+export const updatePartialAlgoliaData = async (data: Data) => {
+  try {
+    const { objectIDs } = await index.partialUpdateObjects([data]);
+    return console.log("\n[ALGOLIA]🎉 Updated record Index", objectIDs);
+  } catch (error) {
+    return console.error("\n[ALGOLIA]🚨 Error updating record Index: ", error);
+  }
 };
